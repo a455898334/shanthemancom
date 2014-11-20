17e3ca312e2f598cef62db2ceaf9deed6045a2e6
Shan Shi <a455898334@gmail.com>
2014-11-19 23:49:19 -0500

    never mind, moving commut off

--- client/app/commut/commut.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('shanmancomApp', ['uiGmapgoogle-maps'])
-  .controller('CommutCtrl', function ($scope) {
-    $scope.message = 'Hello';
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-  });
